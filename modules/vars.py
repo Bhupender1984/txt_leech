d96beb7aa4bbc5b42478f27a6ca037323cf1510f
Bhupender1984 <161750718+Bhupender1984@users.noreply.github.com>
2024-02-29 23:00:28 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "27266898"
+api_hash = "d4d4de851351a4e62628bca4a732e420"
+bot_token = "7045291729:AAG7agBQmF-zSx55_CLng7CDaG0XXzmL5mE"
